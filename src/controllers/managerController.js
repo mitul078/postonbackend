20ddf85d3db1e5e28c9cb754aa2b15d9a5e12c6c
mitul078 <mituljodhani788@gmail.com>
2025-07-28 16:30:09 +0530

    Manager Api Done

--- src/controllers/managerController.js
+++ src/controllers/managerController.js
@@ -4,15 +4,14 @@ const User = require("../models/User")
 
 exports.assignAgent = async (req, res) => {
     const { packageID, agentID } = req.body
-
     try {
         const agent = await User.findOne({ _id: agentID, role: "agent" })
         if (!agent) return res.status(400).json({ message: "Invalid Agent" })
 
         const packageData = await Package.findById(packageID)
-        if(!packageData) return res.status(400).json({message: "Package Invalid"})
+        if (!packageData) return res.status(400).json({ message: "Package Invalid" })
 
-        if(packageData.currentStatus !== "pending"){
+        if (packageData.currentStatus !== "pending") {
             return res.status(400).json({
                 message: "Order is assigned or in transit"
             })
@@ -25,15 +24,15 @@ exports.assignAgent = async (req, res) => {
             statusUpdate: []
         })
 
-        await Package.findByIdAndUpdate(packageID , {
+        await Package.findByIdAndUpdate(packageID, {
             currentStatus: "Assigned",
             assignedAgentInfo: agentID,
-            expectedDelivery: new Date(Date.now() + 3 * 24 * 60 *60  *1000) //3Days
+            expectedDelivery: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000) //3Days
         })
-        
+
         res.status(200).json({
             message: "Assigned to an agent Successfully"
-            ,assignment
+            , assignment
         })
 
     } catch (error) {
@@ -42,3 +41,58 @@ exports.assignAgent = async (req, res) => {
         })
     }
 }
+
+exports.pendingOrders = async (req, res) => {
+    try {
+        const orders = await Package.find({ currentStatus: "pending" }).select("customerID origin destination ")
+        if (orders.length === 0) {
+            res.status(404).json({
+                message: "No Orders Pending"
+            })
+        }
+        res.status(200).json({
+            message: "Fetched successfully",
+            orders
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+}
+exports.assignedOrders = async (req, res) => {
+    try {
+        const orders = await Package.find({ currentStatus: "Assigned" }).select("assignedAgentInfo customerID").populate("assignedAgentInfo" ,"username email")
+
+
+        if (orders.length === 0) {
+            return res.status(404).json({
+                message: "No assigned orders found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Fetched successfully",
+            orders
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.allAgents = async (req, res) => {
+    try {
+
+        const agents = await User.find({ role: "agent" }).select("username email role")
+        res.status(200).json({
+            message: "All Agents Fetched",
+            agents
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+}
+
--- src/routes/manager.js
+++ src/routes/manager.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const {verifyToken , checkRole} = require("../middlewares/authMiddleware") 
-const {assignAgent} = require("../controllers/managerController")
+const {assignAgent, allAgents, assignedOrders, pendingOrders} = require("../controllers/managerController")
 
 router.post("/assign" , verifyToken , checkRole("manager") , assignAgent)
+router.get("/allAgents" , verifyToken , checkRole("manager") , allAgents)
+router.get("/assignedOrders" , verifyToken , checkRole("manager") , assignedOrders)
+router.get("/pendingOrders" , verifyToken , checkRole("manager") , pendingOrders)
+
 
 module.exports = router
